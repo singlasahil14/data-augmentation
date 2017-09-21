2ba75ed4b5155d3422fe8b902f53c11cb80e2e5f
Ridam Batra <ridam@apus.sensu@gmail.com>
2017-09-21 15:01:24 +0530

    Debugged numpy_dataset

--- main.py
+++ main.py
@@ -67,11 +67,11 @@ transform_test = transforms.Compose([
 ])
 
 #trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainset = NumpyDataset(x_train, y_train, transform=transform_train)
+trainset = NumpyDataset(x_train, np.squeeze(y_train, axis=1), transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
 #testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testset = NumpyDataset(x_test, y_test, transform=transform_test)
+testset = NumpyDataset(x_test, np.squeeze(y_test, axis=1), transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
