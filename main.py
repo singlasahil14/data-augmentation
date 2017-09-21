774c31c5e3383cc82717aa826c8bdda248d205c9
Ridam Batra <ridam@apus.sensu@gmail.com>
2017-09-21 17:51:01 +0530

    Add data augmentation from keras

--- main.py
+++ main.py
@@ -21,10 +21,20 @@ from collections import defaultdict
 
 import keras
 from keras import backend as K
-K.set_image_dim_ordering('tf')
+K.set_image_dim_ordering('th')
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
+
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+y_train = np.squeeze(y_train, axis=1)
+y_test = np.squeeze(y_test, axis=1)
+
+train_datagen = ImageDataGenerator(featurewise_center=True, 
+                                   featurewise_std_normalization=True)
+train_datagen.fit(x_train)
+test_datagen = ImageDataGenerator(featurewise_center=True, 
+                                  featurewise_std_normalization=True)
+test_datagen.fit(x_test)
 
 from models import *
 from utils import progress_bar
@@ -33,6 +43,7 @@ from numpy_dataset import NumpyDataset
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--epochs', type=int, default=350, help='number of epochs to train')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 randomhash = ''.join(str(time.time()).split('.'))
 parser.add_argument('--result', type=str,  default=randomhash, help='result path')
@@ -66,13 +77,19 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
+train_batch_size = 128
+num_train_batches = int(np.ceil(len(x_train)/train_batch_size))
 #trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainset = NumpyDataset(x_train, np.squeeze(y_train, axis=1), transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+#trainset = NumpyDataset(x_train, np.squeeze(y_train, axis=1), transform=transform_train)
+#trainloader = torch.utils.data.DataLoader(trainset, batch_size=train_batch_size, shuffle=True, num_workers=2)
+trainloader = train_datagen.flow(x_train, y_train, batch_size=train_batch_size)
 
+test_batch_size = 100
+num_test_batches = int(np.ceil(len(x_test)/test_batch_size))
 #testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testset = NumpyDataset(x_test, np.squeeze(y_test, axis=1), transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+#testset = NumpyDataset(x_test, np.squeeze(y_test, axis=1), transform=transform_test)
+#testloader = torch.utils.data.DataLoader(testset, batch_size=test_batch_size, shuffle=False, num_workers=2)
+testloader = test_datagen.flow(x_test, y_test, batch_size=test_batch_size)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -108,7 +125,7 @@ optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5
 scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[150, 250, 350], gamma=0.1)
 
 train_metrics = defaultdict(list)
-eval_metrics = defaultdict(list)
+test_metrics = defaultdict(list)
 
 # Training
 def train(epoch):
@@ -118,6 +135,8 @@ def train(epoch):
     correct = 0
     total = 0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs = torch.from_numpy(inputs)
+        targets = torch.from_numpy(targets).long()
         if use_cuda:
             inputs, targets = inputs.cuda(), targets.cuda()
         optimizer.zero_grad()
@@ -138,8 +157,12 @@ def train(epoch):
         train_metrics['cross_entropy'].append(loss.data[0])
         train_metrics['accuracy'].append(100.*curr_correct/curr_total)
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+        progress_bar(batch_idx, num_train_batches, 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
             % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        if batch_idx == num_train_batches-1:
+            # we need to break the loop by hand because
+            # the generator loops indefinitely
+            break
 
 def test(epoch):
     global best_acc
@@ -148,6 +171,8 @@ def test(epoch):
     correct = 0
     total = 0
     for batch_idx, (inputs, targets) in enumerate(testloader):
+        inputs = torch.from_numpy(inputs)
+        targets = torch.from_numpy(targets).long()
         if use_cuda:
             inputs, targets = inputs.cuda(), targets.cuda()
         inputs, targets = Variable(inputs, volatile=True), Variable(targets)
@@ -159,13 +184,17 @@ def test(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets.data).cpu().sum()
 
-        progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+        progress_bar(batch_idx, num_test_batches, 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
             % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        if batch_idx == num_test_batches-1:
+            # we need to break the loop by hand because
+            # the generator loops indefinitely
+            break
 
     # Save checkpoint.
     acc = 100.*correct/total
-    eval_metrics['cross_entropy'].append(test_loss/(batch_idx+1))
-    eval_metrics['accuracy'].append(acc)
+    test_metrics['cross_entropy'].append(test_loss/(batch_idx+1))
+    test_metrics['accuracy'].append(acc)
     if acc > best_acc:
         print('Saving..')
         state = {
@@ -178,13 +207,13 @@ def test(epoch):
         torch.save(state, './checkpoint/ckpt.t7')
         best_acc = acc
 
-for epoch in range(0, 350):
+for epoch in range(start_epoch, args.epochs):
     scheduler.step()
     train(epoch)
     test(epoch)
 
     pd_train_metrics = pd.DataFrame(train_metrics)
     pd_train_metrics.to_csv(os.path.join(args.result, 'train_metrics.csv'))
-    pd_eval_metrics = pd.DataFrame(eval_metrics)
-    pd_eval_metrics.to_csv(os.path.join(args.result, 'eval_metrics.csv'))
+    pd_test_metrics = pd.DataFrame(test_metrics)
+    pd_test_metrics.to_csv(os.path.join(args.result, 'test_metrics.csv'))
 
